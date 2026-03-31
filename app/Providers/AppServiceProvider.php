4ad88285112ee37c362504712b204200da037bfe
mansourdev <mansour.dev@gmail.com>
2026-03-31 22:31:12 +0200

    Configure Livewire 3 routes for subfolder support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,14 @@ class AppServiceProvider extends ServiceProvider
         if ($url = config('app.url')) {
             \Illuminate\Support\Facades\URL::forceRootUrl($url);
         }
+
+        // Livewire subfolder support
+        \Livewire\Livewire::setScriptRoute(function ($handle) {
+            return \Illuminate\Support\Facades\Route::get('/app/livewire/livewire.js', $handle);
+        });
+
+        \Livewire\Livewire::setUpdateRoute(function ($handle) {
+            return \Illuminate\Support\Facades\Route::post('/app/livewire/update', $handle);
+        });
     }
 }
